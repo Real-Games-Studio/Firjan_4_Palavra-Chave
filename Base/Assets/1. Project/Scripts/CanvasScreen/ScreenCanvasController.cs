203dcfe33ccf4c9603c70cd5253315277b079613
Zulks <andrescapinelli@hotmail.com>
2025-09-09 14:46:19 -0300

    Fileloader

--- Base/Assets/1. Project/Scripts/CanvasScreen/ScreenCanvasController.cs
+++ Base/Assets/1. Project/Scripts/CanvasScreen/ScreenCanvasController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using TMPro;
-
+using RealGames;
 public class ScreenCanvasController : MonoBehaviour
 {
     public static ScreenCanvasController instance;
+    public AppConfig appConfig;
 
     public string previusScreen;
     public string currentScreen;
@@ -18,13 +19,13 @@ public class ScreenCanvasController : MonoBehaviour
 
     private void OnEnable()
     {
-        // Registra o método CallScreenListner como ouvinte do evento CallScreen
+        // Registra o mï¿½todo CallScreenListner como ouvinte do evento CallScreen
         ScreenManager.CallScreen += OnScreenCall;
 
     }
     private void OnDisable()
     {
-        // Remove o método CallScreenListner como ouvinte do evento CallScreen
+        // Remove o mï¿½todo CallScreenListner como ouvinte do evento CallScreen
         ScreenManager.CallScreen -= OnScreenCall;
 
     }
@@ -38,31 +39,11 @@ public class ScreenCanvasController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyUp(KeyCode.Escape))
-        //{
-        //    if (!DEBUG_CANVAS == null)
-        //    {
-        //        if (DEBUG_CANVAS.alpha == 0)
-        //        {
-        //            DEBUG_CANVAS.alpha = 1;
-        //        }
-        //        else
-        //        {
-        //            DEBUG_CANVAS.alpha = 0;
-        //        }
-        //    }
-        //}
-
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    ScreenManager.CallScreen(inicialScreen);
-        //}
-
-        if (currentScreen != inicialScreen && currentScreen != "chooseMenu")
+        if (currentScreen != inicialScreen)
         {
             inactiveTimer += Time.deltaTime * 1;
 
-            if (inactiveTimer >= 600)
+            if (inactiveTimer >= appConfig.maxInactiveTime)
             {
                 ResetGame();
             }
@@ -71,9 +52,6 @@ public class ScreenCanvasController : MonoBehaviour
         {
             inactiveTimer = 0;
         }
-
-        //if(timeOut != null)
-        //timeOut.SetText($"Time Out:{Mathf.CeilToInt(inactiveTimer)}/{Config.InactiveMaxTime}");
     }
     public void ResetGame()
     {
