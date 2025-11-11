dda005d8efbbb7e916f7ac9fdf0dd4db0cc892ed
Otavio Ribeiro <38363869+PandaCubGames@users.noreply.github.com>
2025-11-11 18:26:59 -0300

    Update button colors and error handling in UI

--- Base/Assets/1. Project/Scripts/CanvasScreen/ErrorUI.cs
+++ Base/Assets/1. Project/Scripts/CanvasScreen/ErrorUI.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _1._Project.Scripts.CanvasScreen
 {
 	public class ErrorUI : MonoBehaviour
 	{
-		private List<GameObject> _errors;
-
+		private List<RawImage> _errors;
+		public Color32 FillColor;
+		public Color32 EmptyColor;
 
 		private void Awake()
 		{
@@ -18,24 +20,24 @@ namespace _1._Project.Scripts.CanvasScreen
 		{
 			for (int i = 0; i < _errors.Count; i++)
 			{
-				_errors[i].SetActive(false);
+				_errors[i].color = EmptyColor;
 			}
 		}
 
 		public void TurnOnError(int errorCount)
 		{
-			_errors[errorCount-1].SetActive(true);
+			_errors[errorCount-1].color = FillColor;
 		}
 
 		private void FillErrors()
 		{
-			_errors = new List<GameObject>();
-			var list = GetComponentsInChildren<Transform>();
+			_errors = new List<RawImage>();
+			var list = GetComponentsInChildren<RawImage>();
 			for (int i = 0; i < list.Length; i++)
 			{
 				if (list[i].gameObject != this.gameObject)
 				{
-					_errors.Add(list[i].gameObject);
+					_errors.Add(list[i]);
 				}
 			}
 		}
